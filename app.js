99ae7f000e98b790918b96eede151ceedd251974
Daniel Clavijo Glez <danielclavijo1313@gmail.com>
2024-07-24 18:48:30 +0100

    Helmet added, fixed linting problems in app.js and deleted unused files

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
-var indexRouter = require("./routes/index").default;
-var usersRouter = require("./routes/users");
+var helmet = require("helmet");
 
 var app = express();
 
@@ -18,9 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use(helmet());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -28,7 +24,7 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};

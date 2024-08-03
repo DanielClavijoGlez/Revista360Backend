64d5978eab5a57c50186bee9d3715c6b4775d58c
Daniel Clavijo Glez <danielclavijo1313@gmail.com>
2024-08-03 14:29:22 +0100

    Created article's routes for API file

---
+++ routes/apiv1/articles.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const router = require("express").Router();
+const asyncHandler = require("express-async-handler");
+const Article = require("../../models/Article");
+
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    
+  })
+);

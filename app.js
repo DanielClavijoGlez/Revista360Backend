2f9795b610476b644dde4a6d67b162b96a86ed52
Daniel Clavijo Glez <danielclavijo1313@gmail.com>
2024-08-05 15:23:50 +0100

    Formatted app.js

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var helmet = require("helmet");
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const helmet = require('helmet')
 
-require("./lib/connectMongoose");
+require('./lib/connectMongoose')
 
-var app = express();
+const app = express()
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(helmet());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(helmet())
 
 // API V1 routes
-app.use("/apiv1/articles", require("./routes/apiv1/articles"));
+app.use('/apiv1/articles', require('./routes/apiv1/articles'))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.json({error: err.message, status: err.status});
-});
+  res.status(err.status || 500)
+  res.json({ error: err.message, status: err.status })
+})
 
-module.exports = app;
+module.exports = app
